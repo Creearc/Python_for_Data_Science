b4fde47da34db734be8a3d7b0d75176fe354d582
Alexandr <knxandr@rambler.ru>
2020-06-19 20:51:06 +0500

    Лабораторная 1 с комментариями
    Задачи.
    
    1) Используя офисное приложение
    или текстовый редактор подготовить
    данные в формате .csv (матрица).
    
    2) Прочитать эти данные в numpy.
    
    3) Найти максимум и минимум.
    Поменять их местами.

--- Lab_1/Lab_1.py
+++ Lab_1/Lab_1.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-x = np.loadtxt(delimiter=';', fname='Lab_1_info.csv')
+# Читаем данные из файла созданного в Excel
+x = np.loadtxt(delimiter=';', fname='Lab_1_info.csv') 
 print(x)
 print()
 
+# Находим минимальное и максимальное значения
 min_elem = np.min(x)
 max_elem = np.max(x)
 
+# Меняем местами максимальные и минимальные значения
+# Чтобы значения не потерлись сначала записываем вместо минимальных элементов максимальное значение увеличенное на единицу
 x[x == min_elem] = max_elem + 1
 x[x == max_elem] = min_elem
 x[x == max_elem + 1] = max_elem
